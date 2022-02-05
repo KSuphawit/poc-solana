060061de8d6fa1ffb4c01ed0914c50e42b697f8d
Suphawit Khruawittawat <suphawit.kh@dv.co.th>
2022-02-05 16:18:10 +0700

    combine init user asscociated with mint

--- programs/poc-solana/src/lib.rs
+++ programs/poc-solana/src/lib.rs
@@ -12,11 +12,11 @@ pub mod poc_solana {
 
     pub fn init_mint(_ctx: Context<InitializeMint>, _token_mint_bump: u8) -> ProgramResult { Ok(()) }
 
-    pub fn init_user_associated_token_acc(_ctx: Context<InitializeUserAssociatedTokenAcc>) -> ProgramResult { Ok(()) }
+    pub fn mint_token(ctx: Context<MintToken>, amount: u64) -> ProgramResult {
+        let (pda, pda_bump) = Pubkey::find_program_address(&["token".as_ref()], ctx.program_id);
 
-    pub fn mint_token(ctx: Context<MintToken>, amount: u64, token_mint_bump: u8) -> ProgramResult {
         anchor_spl::token::mint_to(
-            ctx.accounts.into_mint_to_context().with_signer(&[&["token".as_ref(), &[token_mint_bump]]]),
+            ctx.accounts.into_mint_to_context().with_signer(&[&["token".as_ref(), &[pda_bump]]]),
             amount,
         );
 
@@ -24,10 +24,18 @@ pub mod poc_solana {
     }
 
     pub fn deposit_token(ctx: Context<DepositToken>, amount: u64) -> ProgramResult {
+
+        // let (pda, pda_bump) = Pubkey::find_program_address(&["token".as_ref()], ctx.program_id);
+
         anchor_spl::token::transfer(
             ctx.accounts.into_transfer_context(),
             amount
         );
+
+        // anchor_spl::token::mint_to(
+        //     ctx.accounts.into_mint_to_context().with_signer(&[&["token".as_ref(), &[token_mint_bump]]]),
+        //     amount,
+        // );
 
         Ok(())
     }
@@ -49,13 +57,24 @@ impl<'info> MintToken<'info> {
 impl <'info> DepositToken<'info> {
     fn into_transfer_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
         let cpi_accounts = Transfer {
-            from: self.from_assoc_token_act.to_account_info().clone(),
-            to: self.program_assoc_token_acct.to_account_info().clone(),
+            from: self.user_deposit_token_assoc_token_acct.to_account_info().clone(),
+            to: self.program_deposit_token_assoc_token_acct.to_account_info().clone(),
             authority: self.user.to_account_info().clone()
         };
 
         CpiContext::new(self.token_program.to_account_info().clone(), cpi_accounts)
     }
+
+    // fn into_mint_to_context(&self) -> CpiContext<'_, '_, '_, 'info, MintTo<'info>> {
+    //
+    //     let cpi_accounts = MintTo {
+    //         mint: self.return_token.to_account_info().clone(),
+    //         to: self.user_return_token_assoc_token_acct.to_account_info().clone(),
+    //         authority: self.return_token.to_account_info().clone(),
+    //     };
+    //
+    //     CpiContext::new(self.token_program.to_account_info().clone(), cpi_accounts)
+    // }
 }
 
 #[derive(Accounts)]
@@ -77,7 +96,8 @@ pub struct InitializeMint<'info> {
 }
 
 #[derive(Accounts)]
-pub struct InitializeUserAssociatedTokenAcc<'info> {
+pub struct MintToken<'info> {
+    #[account(mut)]
     pub token_mint: Account<'info, Mint>,
     #[account(
         init_if_needed,
@@ -94,33 +114,26 @@ pub struct InitializeUserAssociatedTokenAcc<'info> {
 }
 
 #[derive(Accounts)]
-pub struct MintToken<'info> {
-    #[account(mut)]
-    pub token_mint: Account<'info, Mint>,
-    #[account(
-        mut,
-        associated_token::mint = token_mint,
-        associated_token::authority = payer
-    )]
-    pub user_assoc_token_acct: Account<'info, TokenAccount>,
-    pub payer: Signer<'info>,
-    pub token_program: Program<'info, Token>,
-    pub rent: Sysvar<'info, Rent>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
 pub struct DepositToken<'info> {
-    pub token_mint: Account<'info, Mint>,
+    pub deposit_token: Account<'info, Mint>,
+    // #[account(mut)]
+    // pub return_token: Account<'info, Mint>,
     #[account(
         init_if_needed,
         payer = user,
-        associated_token::mint = token_mint,
+        associated_token::mint = deposit_token,
         associated_token::authority = program
     )]
-    pub program_assoc_token_acct: Account<'info, TokenAccount>,
+    pub program_deposit_token_assoc_token_acct: Account<'info, TokenAccount>,
     #[account(mut)]
-    pub from_assoc_token_act: Account<'info, TokenAccount>,
+    pub user_deposit_token_assoc_token_acct: Account<'info, TokenAccount>,
+    // #[account(
+    //     init_if_needed,
+    //     payer = user,
+    //     associated_token::mint = return_token,
+    //     associated_token::authority = user
+    // )]
+    // pub user_return_token_assoc_token_acct:  Account<'info, TokenAccount>,
     pub user: Signer<'info>,
     pub program: AccountInfo<'info>,
     pub token_program: Program<'info, Token>,
